919722f532699cd083e702611cda13da8212faf7
Aaryansinghal286 <aaryansinghal28@gmail.com>
2026-07-31 09:48:26 +0530

    class programs

---
+++ OOPs lab.cpp
@@ -0,0 +1,96 @@
+// OOPs code for saving student information using classes and objects
+// #include <iostream>
+// using namespace std;
+
+// class student {
+// public:
+//         string name;
+//         int roll;
+//         float marks;
+//     void display() {
+//         cout << "Name = " << name << endl;
+//         cout << "Roll no. = " << roll << endl;
+//         cout << "Marks = " << marks << endl;
+//     }
+// };
+// int main() {
+//     student s1;
+//     s1.name="john";
+//     s1.roll=100;
+//     s1.marks=99.5;
+//     s1.display();
+//     return 0;
+// }
+// Output :-
+// Name = john
+// Roll no. = 100
+// Marks = 99.5
+
+
+
+
+// OOPs code for saving student information using classes
+// class Student {
+//     public:
+//         string name;
+//         int roll;
+//         float marks;
+//         void input() {
+//             cout << "Enter name: ";
+//             cin >> name;
+//             cout << "Enter roll number: ";
+//             cin >> roll;
+//             cout << "Enter marks: ";
+//             cin >> marks;
+//         }
+//         void display() {
+//             cout << "Name: " << name << endl;
+//             cout << "Roll number: " << roll << endl;
+//             cout << "Marks: " << marks << endl;
+//         }
+// };
+// int main() {
+//     Student s1("john", 100, 99.5);
+//     s1.display();
+//     s1.input();
+//     return 0;
+// }
+
+
+
+
+// using constructor to initialize the values of the class
+// class Student {
+//     public:
+//         string name;
+//         int roll;
+//         float marks;
+//         Student(string n, int r, float m) {
+//             name = n;
+//             roll = r;
+//             marks = m;
+//         }
+//         void display() {
+//             cout << "Name: " << name << endl;
+//             cout << "Roll number: " << roll << endl;
+//             cout << "Marks: " << marks << endl;
+//         }
+// };
+// int main() {
+//     Student s1("john", 100, 99.5);
+//     Student s2("harry", 120, 89);
+//     s1.display();
+//     s2.display();
+//     return 0;
+// }
+// output :-
+// Name: john
+// Roll number: 100
+// Marks: 99.5
+// Name: harry
+// Roll number: 120
+// Marks: 89
+
+
+
+
