12ae6d41b13c74e9b6402fa9beca2488b78417d9
Aaryansinghal286 <aaryansinghal28@gmail.com>
2026-08-29 00:12:01 +0530

    constructors and pillars

--- self/OOPs.cpp
+++ self/OOPs.cpp
@@ -6,26 +6,107 @@
 #include <string>
 using namespace std;
 
+// class teacher{
+// public:
+//     // properties
+//     string name;
+//     string dept;
+//     string subject;
+//     double salary;
+
+//     // methods
+//     void changedept(string newDept){
+//         dept=newDept;
+//     }
+// };
+
+// int main(){
+//     teacher t1; //constructor call 
+//     t1.name="Karan";
+//     t1.dept="CSE";
+//     t1.subject="CS";
+//     t1.salary=50000;
+//     cout<<t1.name<<endl;
+// }
+
+
+
+// Setter and getter functions
+// class teacher{
+// private:
+//     double salary;
+// public:
+//     // properties
+//     string name;
+//     string dept;
+//     string subject;
+
+//     // methods
+//     void changedept(string newDept){
+//         dept=newDept;
+//     }
+//     // Setter
+//     void setSalary(double s){
+//         salary=s;
+//     }
+
+//     // getter
+//     double getSalary(){
+//         return salary;
+//     }
+// };
+// int main(){
+//     teacher t1;
+//     t1.name="Karan";
+//     t1.dept="CSE";
+//     t1.subject="CS";
+//     t1.setSalary(25000);
+//     cout<<t1.name<<endl;
+//     cout<<t1.getSalary()<<endl;
+// }
+
+
+
+// Encapsulation
+// class Account{
+// public:
+//     string AccID;
+//     string username;
+
+// private:
+//     double balance;
+//     string password;
+// }
+
 class teacher{
+private:
+    double salary;
+
 public:
-    // properties
+    // non-parameterized
+    teacher(){
+        dept="CSE";
+    }
+
+    // Parameterized constructor
+    teacher(string n, string d, string s, double sal){
+        this->name=n;  //This (->) pointer
+        dept=d;
+        salary=sal;
+        subject=s;
+    }
     string name;
     string dept;
     string subject;
-    double salary;
 
-    // methods
-    void changedept(string newDept){
-        dept=newDept;
+    void getInfo(){
+        cout<<"name: "<< name <<endl;
+        cout<<"Subject: "<< subject << endl;
+        cout<<"dept: "<<dept<<endl;
     }
 };
-
-int main(){
-    teacher t1;
-    t1.name="Karan";
-    t1.dept="CSE";
-    t1.subject="CS";
-    t1.salary=50000;
-
-    cout<<t1.name<<endl;
+int main() {
+    teacher t1("karan", "CS", "CS", 50000);
+    t1.getInfo();
+    return 0;
 }
--- self/readme.md
+++ self/readme.md
@@ -1,7 +1,46 @@
 <!-- OOPs readme File -->
 
-Access modifiers:
+# Access modifiers:
 1. private -> deafault, data and methdos are accessible only inside class
 2. public -> data & methods are accessible to everyone
 3. Protected -> data & methods are accessible inside class as well as to its derived class
 
+# setter -> to set private values
+# getter -> to get values from private values
+
+
+# Pillars of OOPs
+1. Encapsulation: Wrapping up of data and member functions in single unit called class(ek class banana aur uske andar member aur data func ko implement krna)
+-> helps in data hiding : use private modifier to save sensitive information
+->for interview : encapsulation,its example, data hiding, its example, access modifers and usage with example
+
+2. Abstraction:
+3. Inheritance:
+4. Polymorphism:
+
+<!-- Constructor -->
+# Special method automatically invoked at time of object creation
+->used for initialisation
+->same name as class
+->only called once (automatically), at object creation
+->either made by programmer or compiler itself
+->memory allocation happens when constructor is called (only for objects not for classes)
+
+<!-- Types of constructors -->
+1. Non-parameterized Constructor
+    eg:-
+    teacher(){
+        dept="CS";
+    }
+2. Parameterized Constructor
+    eg:-
+    teacher(string n; string d; string s; double sal;){
+        dept="CS";
+    }
+
+# this (->) Pointer
+
+
+# Constructor Overloading
+constructor with same name but different types
+-> type of polymorphism
