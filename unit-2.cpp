b10d43907e5391ed2cf325e18cbe4244e9646ff8
Aaryansinghal286 <aaryansinghal28@gmail.com>
2026-09-01 15:25:14 +0530

    comparing area function

--- unit-2.cpp
+++ unit-2.cpp
@@ -74,3 +74,51 @@
 // def num func call the are outside the class as inline function
 #include <bits/stdc++.h>
 using namespace std;
+
+
+
+
+
+
+// Create a class rectangle and pass two rectangle objects to a function to find which has the greater area.
+#include <bits/stdc++.h>
+using namespace std;
+
+class rectangle
+{
+private:
+    float area;
+    float perimeter;
+
+public:
+    rectangle(int l, float b)
+    {
+        area = l * b;
+        perimeter = 2 * (l + b);
+    }
+
+    void greaterArea(rectangle rec1, rectangle rec2)
+    {
+        if (rec1.area > rec2.area)
+        {
+            cout << "Area of rec1 is greater " << rec1.area << endl;
+        }
+        else if(rec1.area==rec2.area){
+            cout<<"Area is equal "<<endl;
+        }
+        else
+        {
+            cout << "Area of rec2 is greater " << rec2.area << endl;
+        }
+    }
+};
+
+int main()
+{
+    rectangle rec1(4, 3);
+    rectangle rec2(3, 4);
+
+    rec1.greaterArea(rec1, rec2);
+
+    return 0;
+}
